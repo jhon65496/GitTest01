16d8ef1b98e5424a98f50c0831c34ea02b757675
jhon65496 <it1233@yandex.ru>
2023-04-17 16:14:18 +0300

    Изменния - 1 | branc1-2. Изм.1

--- GitTest01/Program.cs
+++ GitTest01/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GitTest01
 {
-    // Изменния - 1
+    // Изменния - 1 | branc1-2. Изм.1
     internal class Program
     {
         static void Main(string[] args)
